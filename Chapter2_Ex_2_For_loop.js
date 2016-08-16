4177e0bef053e41fb254f2c97dac9c21b08dcdba
Angad Gill <angadpalsingh92@gmail.com>
2016-08-16 21:31:32 +0530

    Exercise 2

---
+++ Chapter2_Ex_2_For_loop.js
@@ -0,0 +1,17 @@
+/* Fizz buzz program, prints 'Fizz' for every multiple of 3(and not 5)
+   , 'Buzz' for every multiple of 5( and not 3), and FizzBuzz for
+   multiples of 5 and 3 , between 1 and 100*/
+
+for ( var number = 1 ; number <= 100 ; number++){
+
+  if (( number % 3 === 0 ) && ( number % 5 != 0 ))
+    console.log ( 'Fizz' ) ;
+  else if (( number % 5 === 0) && ( number % 3 != 0 ))
+    console.log ( 'Buzz' ) ;
+  else if ((number % 3 === 0) && (number % 5 === 0))
+    console.log ( 'FizzBuzz' ) ;
+  else
+    continue;
+
+
+}
