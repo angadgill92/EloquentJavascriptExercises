c1977bb94e207db9411288c00bbf1c4eb2f46418
Angad Gill <angadpalsingh92@gmail.com>
2016-08-23 11:48:36 +0530

    This commit adds a map function I wrote

---
+++ myMapFunction.js
@@ -0,0 +1,14 @@
+function myMap(iterable, functionToBeApplied) {
+    /*Takes an iterable and a  function as arguments, applies the 
+    function to each element of the iterable and returns a new transformed 
+    iterable*/
+
+    var returnedIterable = iterable; 
+    //Create a local copy of the iterable to operate on.
+
+    for (var i = 0; i < iterable.length; i++) {
+
+        returnedIterable[i] = functionToBeApplied(returnedIterable[i]);
+    }
+    return returnedIterable;
+}
