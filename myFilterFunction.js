787a4ab020de31fd9eefd46b221b176d03d54cbe
Angad Gill <angadpalsingh92@gmail.com>
2016-09-27 20:06:43 +0530

    Changed Function Description

--- myFilterFunction.js
+++ myFilterFunction.js
@@ -1,11 +1,12 @@
-const myFilter = (iterable, test) => {
-  /*Takes an iterable and a  function as arguments, applies the
-  function to each element of the iterable and returns a new transformed
-  iterable*/
+const myFilter = (iterable, predicate) => {
+  /*Takes an iterable and a predicate as arguments, and filters out the elements
+   based on the predicate*/
   if (iterable.length === 0) {
     return []
   } else {
-    return (test(iterable[0])) ? [iterable[0]].concat(myFilter(iterable.slice(1), test)) : myFilter(iterable.slice(1), test)
+    return (predicate(iterable[0]))
+              ? [iterable[0]].concat(myFilter(iterable.slice(1), predicate))
+              : myFilter(iterable.slice(1), predicate)
   }
 }
-console.log (myFilter ([1, 2, 3, 4, 1], a => a !== 1))
+console.log (myFilter ([1, 2, 3, 4, 1], a => a % 2 === 0))
